86be0d31ed133f79c805f27c66f8c57c85bf83ea
leandro ostruka <leandro.sakae@hotmail.com>
2023-12-07 15:37:09 -0300

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.operBox = new System.Windows.Forms.TextBox();
+            this.CE = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.mult = new System.Windows.Forms.Button();
+            this.sub = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.one = new System.Windows.Forms.Button();
+            this.dot = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.som = new System.Windows.Forms.Button();
+            this.ResultBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // operBox
+            // 
+            this.operBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.operBox.Enabled = false;
+            this.operBox.Font = new System.Drawing.Font("Courier New", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.operBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.operBox.Location = new System.Drawing.Point(11, 69);
+            this.operBox.Name = "operBox";
+            this.operBox.Size = new System.Drawing.Size(376, 44);
+            this.operBox.TabIndex = 0;
+            this.operBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // CE
+            // 
+            this.CE.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CE.Location = new System.Drawing.Point(11, 119);
+            this.CE.Name = "CE";
+            this.CE.Size = new System.Drawing.Size(89, 89);
+            this.CE.TabIndex = 1;
+            this.CE.Text = "CE";
+            this.CE.UseVisualStyleBackColor = true;
+            // 
+            // C
+            // 
+            this.C.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C.Location = new System.Drawing.Point(106, 119);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(89, 89);
+            this.C.TabIndex = 2;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
+            // 
+            // div
+            // 
+            this.div.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.div.Location = new System.Drawing.Point(203, 119);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(89, 89);
+            this.div.TabIndex = 3;
+            this.div.Text = "÷";
+            this.div.UseVisualStyleBackColor = true;
+            // 
+            // mult
+            // 
+            this.mult.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mult.Location = new System.Drawing.Point(298, 119);
+            this.mult.Name = "mult";
+            this.mult.Size = new System.Drawing.Size(89, 89);
+            this.mult.TabIndex = 4;
+            this.mult.Text = "X";
+            this.mult.UseVisualStyleBackColor = true;
+            // 
+            // sub
+            // 
+            this.sub.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sub.Location = new System.Drawing.Point(298, 214);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(89, 89);
+            this.sub.TabIndex = 8;
+            this.sub.Text = "-";
+            this.sub.UseVisualStyleBackColor = true;
+            // 
+            // nine
+            // 
+            this.nine.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nine.Location = new System.Drawing.Point(203, 214);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(89, 89);
+            this.nine.TabIndex = 7;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = true;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // eight
+            // 
+            this.eight.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eight.Location = new System.Drawing.Point(106, 214);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(89, 89);
+            this.eight.TabIndex = 6;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = true;
+            this.eight.Click += new System.EventHandler(this.eight_Click);
+            // 
+            // seven
+            // 
+            this.seven.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seven.Location = new System.Drawing.Point(11, 214);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(89, 89);
+            this.seven.TabIndex = 5;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = true;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // equal
+            // 
+            this.equal.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equal.Location = new System.Drawing.Point(298, 404);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(89, 184);
+            this.equal.TabIndex = 16;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            // 
+            // three
+            // 
+            this.three.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.three.Location = new System.Drawing.Point(203, 404);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(89, 89);
+            this.three.TabIndex = 15;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = true;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // two
+            // 
+            this.two.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.two.Location = new System.Drawing.Point(106, 404);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(89, 89);
+            this.two.TabIndex = 14;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // one
+            // 
+            this.one.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.one.Location = new System.Drawing.Point(11, 404);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(89, 89);
+            this.one.TabIndex = 13;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // dot
+            // 
+            this.dot.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dot.Location = new System.Drawing.Point(205, 499);
+            this.dot.Name = "dot";
+            this.dot.Size = new System.Drawing.Size(89, 89);
+            this.dot.TabIndex = 19;
+            this.dot.Text = ".";
+            this.dot.UseVisualStyleBackColor = true;
+            this.dot.Click += new System.EventHandler(this.dot_Click);
+            // 
+            // zero
+            // 
+            this.zero.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zero.Location = new System.Drawing.Point(11, 499);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(186, 89);
+            this.zero.TabIndex = 18;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // four
+            // 
+            this.four.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.four.Location = new System.Drawing.Point(11, 309);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(89, 89);
+            this.four.TabIndex = 9;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = true;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // five
+            // 
+            this.five.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.five.Location = new System.Drawing.Point(106, 309);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(89, 89);
+            this.five.TabIndex = 10;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = true;
+            this.five.Click += new System.EventHandler(this.five_Click);
+            // 
+            // six
+            // 
+            this.six.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.six.Location = new System.Drawing.Point(203, 309);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(89, 89);
+            this.six.TabIndex = 11;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = true;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // som
+            // 
+            this.som.Font = new System.Drawing.Font("Courier New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.som.Location = new System.Drawing.Point(298, 309);
+            this.som.Name = "som";
+            this.som.Size = new System.Drawing.Size(89, 89);
+            this.som.TabIndex = 12;
+            this.som.Text = "+";
+            this.som.UseVisualStyleBackColor = true;
+            // 
+            // ResultBox
+            // 
+            this.ResultBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ResultBox.Enabled = false;
+            this.ResultBox.Font = new System.Drawing.Font("Courier New", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.ResultBox.Location = new System.Drawing.Point(11, 12);
+            this.ResultBox.Multiline = true;
+            this.ResultBox.Name = "ResultBox";
+            this.ResultBox.Size = new System.Drawing.Size(376, 63);
+            this.ResultBox.TabIndex = 20;
+            this.ResultBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 599);
+            this.Controls.Add(this.ResultBox);
+            this.Controls.Add(this.dot);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.som);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.mult);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.CE);
+            this.Controls.Add(this.operBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ResultBox;
+        private System.Windows.Forms.Button CE;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button mult;
+        private System.Windows.Forms.Button sub;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button equal;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button dot;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button som;
+        private System.Windows.Forms.TextBox operBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        decimal val1 = 0, val2 = 0;
+        string operation = "";
+        bool dotVal1 = false, dotVal2 = false; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            operBox.Text += "0";
+        }
+
+        private void one_Click(object sender, EventArgs e)
+        {
+            operBox.Text += "1";
+        }
+
+        private void two_Click(object sender, EventArgs e)
+        {
+            operBox.Text += "2";
+        }
+
+        private void three_Click(object sender, EventArgs e)
+        {
+            operBox.Text += "3";
+        }
+
+        private void four_Click(object sender, EventArgs e)
+        {
+            operBox.Text += "4";
+        }
+
+        private void five_Click(object sender, EventArgs e)
+        {
+            operBox.Text += "5";
+        }
+
+        private void six_Click(object sender, EventArgs e)
+        {
+            operBox.Text += "6";
+        }
+
+        private void seven_Click(object sender, EventArgs e)
+        {
+            operBox.Text += "7";
+        }
+
+        private void eight_Click(object sender, EventArgs e)
+        {
+            operBox.Text += "8";
+        }
+
+        private void nine_Click(object sender, EventArgs e)
+        {
+            operBox.Text += "9";
+        }
+        
+        private void dot_Click(object sender, EventArgs e)
+        {
+            if (dotVal1 == false && val1 == 0)
+            {
+                operBox.Text += ".";
+                dotVal1 = true;
+            }
+            else if (dotVal2 == false && val2 == 0);
+        }
+    }
+}
